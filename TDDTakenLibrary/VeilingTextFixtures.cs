b1da3932a9947c15a80f81a38d1c07fe2ce139c0
TinoSchuerewegen <Tino.Schuerewegen@gmail.com>
2020-07-12 20:41:14 +0200

    12/07/2020-20.41

---
+++ TDDTakenLibrary/VeilingTextFixtures.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+
+namespace TDDTakenLibrary
+{
+    public class VeilingTextFixtures
+    {
+        private decimal hoogsteBod;
+
+        public decimal HoogsteBod
+        {
+            get
+            {
+                return hoogsteBod;
+            }
+        }
+
+        public void DoeBod(decimal bod)
+        {
+            if (bod > hoogsteBod)
+            {
+                hoogsteBod = bod;
+            }
+        }
+    }
+}
---
+++ TDDTakenLibraryTest/VeilingTextFixturesTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTakenLibrary;
+
+namespace TDDTakenLibraryTest
+{
+    [TestClass]
+    public class VeilingTextFixturesTest
+    {
+        private Veiling veiling;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            veiling = new Veiling();
+        }
+
+        [TestMethod]
+        public void HetHoogsteBodVanEenNieuweVeilingIsNul()
+        {
+            //var veiling = new Veiling();
+            Assert.AreEqual(decimal.Zero, veiling.HoogsteBod);
+        }
+
+        [TestMethod]
+        public void HetHoogsteBodNaEenEersteBodIsHetBedragVanDatBod()
+        {
+            //var veiling = new Veiling();
+            veiling.DoeBod(100);
+            Assert.AreEqual(100m, veiling.HoogsteBod);
+        }
+
+        [TestMethod]
+        public void NaMeerdereBiedingenIsHetHoogsteBodGelijkAanHetBedragVanDitBod()
+        {
+            //var veiling = new Veiling();
+            veiling.DoeBod(100m);
+            veiling.DoeBod(200m);
+            veiling.DoeBod(150m);
+            Assert.AreEqual(200m, veiling.HoogsteBod);
+        }
+    }
+}
