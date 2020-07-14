a719182169e34b83b655d47d57f7c2deeb7c3669
TinoSchuerewegen <Tino.Schuerewegen@gmail.com>
2020-07-14 17:24:52 +0200

    14/07/2020-17.24

---
+++ TDDTakenLibrary/ISBN.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Resources;
+using System.Text.Json;
+
+namespace TDDTakenLibrary
+{
+    public class ISBN
+    {
+        public const long MaxWaarde13Cijfers = 9999999999999L;
+        public const long MinWaarde13Cijfers = 1000000000000L;
+        private long nummer;
+
+        public ISBN(long nummer)
+        {
+            //throw new NotImplementedException();
+            if(nummer<MinWaarde13Cijfers || nummer > MaxWaarde13Cijfers)
+            {
+                throw new ArgumentException();
+            }
+
+            var somEvenCijfers = 0L;
+            var somOnevenCijfers = 0L;
+            var teVerwerkenCijfers = nummer / 10;
+
+            for (int teller = 0; teller!=6; teller++)
+            {
+                somEvenCijfers += teVerwerkenCijfers % 10;   // je begint met 12 cijfers, je deelt dit door 10 om een cijfer na de 
+                teVerwerkenCijfers /= 10;                    // komma te krijgen door restdeling toe te passen krijg je dit cijfer. En dan deel je het nummer door 10 
+                somOnevenCijfers += teVerwerkenCijfers % 10; //om naar 11 cijfers te gaan, enz...
+                teVerwerkenCijfers /= 10;
+            }
+
+            var controleGetal = somEvenCijfers * 3 + somOnevenCijfers; // om het controlegetal te verkrijgen doe je som even cijfers maal 3 + som oneven cijfers
+            var naastGelegenHogerTiental = controleGetal - controleGetal % 10 + 10; // 138 - 8 (restdeling 10) + 10 geeft je het naast hoger gelegen tiental
+            var verschil = naastGelegenHogerTiental - controleGetal;
+            var laatsteCijfer = nummer % 10;
+
+            if (verschil == 10)
+            {
+                if (laatsteCijfer != 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            else
+            {
+                if (laatsteCijfer != verschil)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            this.nummer = nummer;
+        }
+
+        public override string ToString()
+        {
+            //throw new NotImplementedException();
+            return nummer.ToString();
+        }
+    }
+}
---
+++ TDDTakenLibraryTest/ISBNTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTakenLibrary;
+
+namespace TDDTakenLibraryTest
+{
+    [TestClass]
+    public class ISBNTest
+    {
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void NummerMet12CijfersIsFout()
+        {
+            new ISBN(978902743964L);
+        }
+
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void NummerMet14CijfersIsFout()
+        {
+            new ISBN(97890274396422L);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNegatiefNummerIsFout()
+        {
+            new ISBN(-97890727439642L);
+        }
+
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void NummerMet0IsFout()
+        {
+            new ISBN(0);
+        }
+
+        [TestMethod,ExpectedException(typeof(ArgumentException))]
+        public void NummerMet13CijfersMetVerkeerdeControleNr()
+        {
+            new ISBN(8789027439642L);
+        }
+
+        [TestMethod]
+        public void NummerMet13CijfersMetCorrecteControleNr()
+        {
+            new ISBN(9789027439642L);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NummerMet13CijfersMetVerkeerdeControleNr0()
+        {
+            new ISBN(7789227439640L);
+        }
+
+        [TestMethod]
+        public void NummerMet13CijfersMetCorrecteControleNr0()
+        {
+            new ISBN(9789227439640L);
+        }
+    }
+}
